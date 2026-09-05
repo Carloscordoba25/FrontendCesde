c147fc837d54fe31fdeae163ac9d3f7ffd166af2
Carlos Cordoba <cacordoba@cesde.net>
2026-09-05 08:20:31 -0500

    version 1.1 cajero

--- TallerCajeroElectronico/cajeroElectronico.js
+++ TallerCajeroElectronico/cajeroElectronico.js
@@ -1,9 +1,11 @@
 
+let usuarios = [];
 let totalRetiros = 0;
 let totalConsignaciones = 0;
 let saldo = 0;
-let nombre = "";
-let clave = 0;
+let ingresaNombre = "";
+let ingresaClave = "";
+
 
 function menuPrincipal(){
 let opcion = 0;
@@ -18,36 +20,52 @@ console.log("A continiacion, el Menú Principal");
     
     switch(opcion){
         case 1 : transacciones(); break;
-        case 2 : pedirdatos(); break;
+        case 2 : registrar(); break;
         case 3 : console.log("SALIENDO DEL LA SUCURSAL HASTA PRONTO 👍"); break;
         default : console.log("🚧 OPCION INVALIDA 🚧 "); break;
         }
     }
 }
+menuPrincipal()
 
-menuPrincipal();
+ function crearUsuario(id, nombre, correo, mensaje){
+     let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
- function crearUsuario(id, nombre, correo, clave, validarClave, mensaje){
-    console.log(`++++NUEVO USUARIO++++
-        ID -------> ${id}
-        NOMBRE ---> ${nombre}
-        EMAIL ----> ${correo}
-        CLAVE ----> ${clave}
-        detalle -> ${mensaje}`);
+    for (let i = 0; i < usuarios.length; i++) {
+        console.log("Usuario " + (i + 1));
+        console.log("ID: " + usuarios[i].id);
+        console.log("Nombre: " + usuarios[i].nombre);
+        console.log("Correo: " + usuarios[i].correo);
+        console.log("-------------------------");
+    }
 
- }
+ }2
 
 //esta es la funcion que usa prompt para agregar usuarios nuevos
-function pedirdatos(){
+function registrar(){
     console.log("Vamos a crear un usuario nuevo");
     let id = prompt ("Ingresa tu Numero de Identificacion");
-    nombre = prompt ("Ingresa tu nombre de usuario");
+    let nombre = prompt ("Ingresa tu nombre de usuario");
     let correo = prompt ("Ingresa tu email");
-    clave = parseInt(prompt ("Ingresa una clave de 4 digitos"));
-    let validarClave = parseInt (prompt ("Ingresa de nuevo tu clave"));
+    let clave = prompt ("Ingresa una clave de 4 digitos");
+    let validarClave = prompt ("Ingresa de nuevo tu clave");
+
+    // Recuperar lista actual de usuarios (si existe) se hace la consulta al  arreglo usuarios[]
+    usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    //Crear objeto usuario y se redefine cada vez que creo uno nuevo
+    let nuevoUsuario = { id: id, nombre: nombre, correo: correo, clave: clave, validarClave: validarClave };
+
+    // Agregamos el objeto al arreglo con .push()
+    // .push() añade el elemento al final del array
+    usuarios.push(nuevoUsuario)
+
+    // Guardamos el arreglo completo en Local Storage
+    // JSON.stringify convierte el array en texto para poder almacenarlo
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
     
     mensaje = clave === validarClave ? "Usuario creado con Exito" : "La clave no coincide";
-    crearUsuario(id, nombre, correo, clave, validarClave, mensaje)
+    crearUsuario(id, nombre, correo, mensaje)
     console.log("De vuelta al Menu Principal");
 }
     
@@ -74,48 +92,90 @@ function transacciones(){
 
 
 function retiros(){
-    let ingresaNombre = prompt("Ingrese su Nombre de Usuario");
-    let ingresaClave = parseInt(prompt("Ingrese su Clave"));
+    ingresaNombre = prompt("Ingrese su Nombre de Usuario");
+    ingresaClave = prompt("Ingrese su Clave");
+    let retiros = parseFloat(prompt("Ingrese monto a Retirar"))
 
-    if (ingresaNombre === nombre && ingresaClave === clave){
-1
-    let retiro = parseFloat(prompt(`Cuanto desea retirar`));
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-    if (retiro <= saldo){
-    saldo -= retiro;
-    totalRetiros += retiro;
-    console.log(`Acabas de retirar ${retiro}`);}
-    else {
-    console.log("Fondos Insuficientes");
-        }
-    return saldo;
-    } else{alert("Usuario o contraseña incorrectos");}
+    
+    let usuario = usuarios.find(user => user.nombre === ingresaNombre && user.clave === ingresaClave);
+
+    
+    if (usuario && retiros > 0 ) {
+        saldo -= retiros;
+        totalRetiros += retiros;
+
+        console.log(`Bienvenido ${usuario.nombre} acabas de retirar ${retiros} 
+            ve a la seccion Consultar saldo para ver tu monto actual `);
+    } else {
+        
+        console.log("Usuario o contraseña incorrectos");
+    }
 }
 
 
 function consultarSaldo(){
-    console.log(`Tu saldo actual es: ${saldo}`);
-    return saldo;
+    ingresaNombre = prompt("Ingrese su Nombre de Usuario");
+    ingresaClave = prompt("Ingrese su Clave");
+
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    let usuario = usuarios.find(user => user.nombre === ingresaNombre && user.clave === ingresaClave);
+
+    if (usuario) {
+        console.log(`Bienvenido ${usuario.nombre} Su saldo es ${saldo}`);
+    } else {
+       
+        console.log("Usuario o contraseña incorrectos 🚧");
+    }
+    
 }
 
 
 function consignacion (){
-    let consignar = parseFloat(prompt(`Cuanto desea consignar?`))
-    if (consignar > 0 ){
+    ingresaNombre = prompt("Ingrese su Nombre de Usuario");
+    ingresaClave = prompt("Ingrese su Clave");
+    let consignar = parseFloat(prompt("Que monto desea consignar?"))
+
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    let usuario = usuarios.find(user => user.nombre === ingresaNombre && user.clave === ingresaClave);
+
+    if (usuario) {
         saldo += consignar;
         totalConsignaciones += consignar;
-        console.log(`Acabas de Consignar ${consignar} 🤑`);
-    }else{console.log("Monto Invalido 🚨");}
-    return saldo;
+
+        console.log(`Bienvenido ${usuario.nombre} Acabas de consignar ${consignar}`);
+
+    } else if (consignar <= 0) {
+        
+        console.log(`${consignar} es Monto Invalido 🚨 debe ser un monto mayor a 0 pesos`);
+    }else {
+        console.log("Usuario o contraseña incorrectos 🚧")
+    }
+
 
 }
 
 function consultarMovimientos(){
-    console.log(`Has relizado los siguientes Movimientos
-        Retiros ${totalRetiros}
-        consignaciones ${totalConsignaciones}`);
-}
+    ingresaNombre = prompt("Ingrese su Nombre de Usuario");
+    ingresaClave = prompt("Ingrese su Clave");
 
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    let usuario = usuarios.find(user => user.nombre === ingresaNombre && user.clave === ingresaClave);
+
+    if (usuario) {
+        console.log(`Bienvenido ${usuario.nombre} Su Movimientos han sido:
+            Retiros ${totalRetiros}
+            consignaciones ${totalConsignaciones}`);
+    } else {
+       
+        console.log("Usuario o contraseña incorrectos 🚧");
+    }
+}
+menuPrincipal();
 
 
 
