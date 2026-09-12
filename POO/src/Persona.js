0b11e6c0c806762e1c5b347c097fadb8bcf068ac
Carlos Cordoba <cacordoba@cesde.net>
2026-09-12 14:56:09 -0500

    Exportacion e Importacion

--- POO/src/Persona.js
+++ POO/src/Persona.js
@@ -1,5 +1,5 @@
-// 1. nombre de la clase primera letra mayuscula
-class Persona{
+// 1. nombre de la clase primera letra mayuscula, export es para enviar datos a otras clases
+ export class Persona{
 // 2. propiedades (#) para crear atributos privados
 
 #identificacion;
@@ -21,13 +21,33 @@ constructor(identificacion, nombres, apellidos, edads, correo, celular, estatura
     this.#estatura       = estatura
 }
 
-
-
 // get y set
+get identificacion(){return this.#identificacion}
+get nombres()       {return this.#nombres}
+get apellidos()     {return this.#apellidos}
+get edads()         {return this.#edads}
+get correo()        {return this.#correo}
+get celular()       {return this.#celular}
+get estatura()      {return this.#estatura}
+
 
 
 // metodos
-    constructor(){
+  
+   mostrarDatos() {
+    return console.log(`
+    +++++++++DATOS DE LA PERSONA+++++++++
+    Identificacion -> ${this.identificacion}
+    Nombres --------> ${this.#nombres}
+    Apellidos ------> ${this.#apellidos}
+    Edads ----------> ${this.#edads}
+    Correo ---------> ${this.#correo}
+    Celular --------> ${this.#celular}
+    Estatura -------> ${this.#estatura}`)
+   }
 
-    }
+   
+   actualizarDatos() {
+    console.log("En construccion");
+   }
 }
